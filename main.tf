8c3c53e59945428836f52f519c90aeb616da067f
Naganathan05 <umaiyalramesh@gmail.com>
2024-01-13 16:14:01 +0530

    Updated Security Group Scripts

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
-# AWS configuration
-provider "aws" {
-  region = "${var.region}"
-}
-
+// Configuring Virtual Private Cloud
 module "vpc" {
   source = "./modules/vpc"
   vpc_cidr = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
+
+// Security Group for the instance
+module "sg" {
+  source = "./modules/sg"
+  vpc_id = module.vpc.vpc_id
+}
+
+// Configuring the EC2 instance
